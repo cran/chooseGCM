3d08e80646ef70e069fca07022414746b79f987f
Luíz Esser <luizesser@gmail.com>
2026-01-09 00:20:02 +0000

    version 1.3

--- R/env_gcms.R
+++ R/env_gcms.R
@@ -18,9 +18,9 @@
 #'
 #' @examples
 #' var_names <- c("bio_1", "bio_12")
-#' s <- import_gcms(system.file("extdata", package = "chooseGCM"), var_names = var_names)
-#' study_area <- terra::ext(c(-80, -30, -50, 10)) |> terra::vect(crs="epsg:4326")
-#' env_gcms(s, var_names, study_area, highlight = "sum")
+#' s <- import_gcms(system.file("extdata", package = "chooseGCM"), var_names = var_names)[1:5]
+#' study_area <- terra::ext(c(-80, -70, -50, -40)) |>
+#'   terra::vect(crs="+proj=longlat +datum=WGS84 +no_defs")
 #' env_gcms(s, var_names, study_area, highlight = c("ae", "ch", "cr"))
 #'
 #' @import checkmate
